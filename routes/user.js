c6e8ab5849fa0b4eed5e4ce032811e68b6625c97
larsnordstrom <larsnordstrom@me.com>
2019-03-15 14:26:48 +0100

    Added route for getting all highscores.

--- routes/user.js
+++ routes/user.js
@@ -10,4 +10,22 @@ router.get("/api/user", (req, res) => {
     res.send("<h1>Home User route</h1>");
 })
 
+// Get all scores
+router.get("/api/users/all", (req, res) => {
+    let queryString = "SELECT * FROM highscores ORDER BY score DESC";
+    db().query(queryString, (error, rows, fields) => {
+        
+        if(error) {
+            res.sendStatus(500);
+            res.end();
+            return;
+        };
+        
+       const data = rows.map( (row) => {
+           return {Id: row.id, Tag: row.gamertag, Score: row.score};
+       });
+       res.json(data);
+    });
+});
+
 module.exports = router;
